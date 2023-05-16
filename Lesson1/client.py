967cdf17afe6f5ee380152cce2e93ea7a3ae06cf
Vladimir <ecrvmal@yandex.ru>
2023-05-16 12:23:54 -0700

    Lesson1 completed

--- Lesson1/client.py
+++ Lesson1/client.py
@@ -3,5 +3,16 @@ args = ['ping', 'google.com']
 
 subproc_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
 
+def update_line(line):
+    line += ' checked'
+    return line
+
 for line in subproc_ping.stdout:
-    print(line)
+    # print(line)      # output as bytes
+    # print(line.decode('cp866'))      # output as string
+    l = line.decode('cp866')
+    l = update_line(l)
+    print(type(l), l)
+    encoded_data = l.encode('UTF-8')
+    print(encoded_data)
+
---
+++ Lesson1/client3.py
@@ -0,0 +1,3 @@
+with open('test.txt','r', encoding='UTF-8') as f:
+    for line in f:
+        print(line)
